08587c3f60361ef5fcf14f3eba74826485ac48bc
Andreu <andreusalbos@gmail.com>
2017-07-03 12:52:22 +0200

    again modules

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+var	mongoose = require("mongoose"),
+	user = require("./models/user")
+
+
+mongoose.connect('mongodb://localhost/listingDb')
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we are connected');
+});
+
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendfile(__dirname + 'public/index.html');
 })
 
 app.listen(80, function () {
