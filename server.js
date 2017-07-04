4f57c4eae0fb70e517447bd5d36db6b02fccab0b
Andreu <andreusalbos@gmail.com>
2017-07-04 13:01:25 +0200

    =a ver que tal

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
-var	mongoose = require("mongoose"),
-	user = require("./models/user")
+var	mongoose = require("mongoose")
+var User = require("./models/user")
+var bodyParser = require('body-parser')
 
 
-mongoose.connect('mongodb://localhost/listingDb')
+//mongoose.connect('mongodb://localhost/listingDb')
+mongoose.connect('mongodb://andreukalu:Assaigdedilluns1/@cluster0-shard-00-00-dw4bz.mongodb.net:27017,cluster0-shard-00-01-dw4bz.mongodb.net:27017,cluster0-shard-00-02-dw4bz.mongodb.net:27017/listingDb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 
 var db = mongoose.connection;
@@ -15,10 +17,33 @@ db.once('open', function() {
 
 const app = express()
 
+app.use(express.static('public'))
+   /* 'default', 'short', 'tiny', 'dev' */
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + 'public/index.html');
+	
+  res.sendFile(__dirname+'/public/index.html')
 })
+
+app.get('/users', function (req, res) {
+	var user = new User();
+	user.username = req.body.username;
+	user.password = req.body.password;
+	user.name = req.body.password;
+	user.save(function(err){
+		if(err){
+			res.send("error");
+		}else{
+			res.send("user created");
+		}
+	})
+})
+
+
 
 app.listen(80, function () {
   console.log('Example app listening on port 80')
+  console.log(__dirname)
 })
